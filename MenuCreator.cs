0d8d47d2ba6559059e6383abb11c7cae5f26b3d3
Virtuesky <virtuesky.0520@gmail.com>
2023-09-23 17:13:15 +0700

    Update MenuCreator.cs

--- MenuCreator.cs
+++ MenuCreator.cs
@@ -9,29 +9,12 @@ namespace VirtueSky.Ads
 {
     public class MenuCreator : EditorWindow
     {
-        [MenuItem("Tools/AdSetting %E", false)]
+        [MenuItem("GameControl/AdSetting %E", false)]
         public static void MenuOpenAdSettings()
         {
-            var adSetting = AssetUtils.FindAssetAtFolder<AdSetting>(new string[] { "Assets" }).FirstOrDefault();
-            if (adSetting == null)
-            {
-                ScriptableSetting.CreateSettingAssets<AdSetting>();
-                adSetting = AssetUtils.FindAssetAtFolder<AdSetting>(new string[] { "Assets" }).FirstOrDefault();
-            }
-
+            var adSetting = ScriptableSetting.CreateAndGetScriptableAsset<AdSetting>();
             Selection.activeObject = adSetting;
             EditorUtility.FocusProjectWindow();
-        }
-
-        public static T CreateScriptableSetting<T>() where T : ScriptableObject
-        {
-            var so = AssetUtils.FindAssetAtFolder<T>(new string[] { "Assets" }).FirstOrDefault();
-            if (so == null)
-            {
-                ScriptableSetting.CreateSettingAssets<T>();
-                so = AssetUtils.FindAssetAtFolder<T>(new string[] { "Assets" }).FirstOrDefault();
-            }
-            return so;
         }
     }
 }
