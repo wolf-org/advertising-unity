aee3a9e3b840ad1858b27cc4cff1b1ccc56d8dc6
Virtuesky <virtuesky.0520@gmail.com>
2023-10-14 19:27:51 +0700

    update adsize for admob banner

--- Admob/AdmodUnitVariable/AdmobBannerVariable.cs
+++ Admob/AdmodUnitVariable/AdmobBannerVariable.cs
@@ -75,7 +75,12 @@ namespace VirtueSky.Ads
         {
             switch (size)
             {
-                case BannerSize.Adaptive: return AdSize.GetCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
+                case BannerSize.Adaptive:
+                    return AdSize.GetCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
+                case BannerSize.MediumRectangle: return AdSize.MediumRectangle;
+                case BannerSize.Leaderboard: return AdSize.Leaderboard;
+                case BannerSize.IABBanner: return AdSize.IABBanner;
+                //case BannerSize.SmartBanner: return AdSize.SmartBanner;
                 default: return AdSize.Banner;
             }
         }
